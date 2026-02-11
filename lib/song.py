3c18104b902b60a519df3d1eaf58fa6930f41fa9
Sanaya James <sanaya.j.james@gmail.com>
2026-02-11 16:26:52 -0700

    initialized structure; __init__ and bare bones of class methods. 2 tests passed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,36 @@
-class Song:
-    pass
+class Song():
+	# Class attributes
+	count = 0
+	genres = []
+	artists = []
+	genre_count = {}
+	artists_count = {}
+
+	def __init__(self, name, artist, genre):
+		self.name = name
+		self.artist = artist
+		self.genre = genre
+		Song.count += 1 # increment when a song is added
+
+	# @classmethod
+	# def add_song_to_count(cls):
+	# 	cls.count += 1
+
+	@classmethod
+	def add_to_genres(cls, genre):
+		pass
+
+	@classmethod
+	def add_to_artists(cls):
+		pass
+
+	@classmethod
+	def add_to_genre_count(cls):
+		pass
+
+	@classmethod
+	def add_to_artists_count(cls):
+		pass
+
+song1 = Song("A Long Walk", "Jill Scott", "R&B")
+print(Song.count)
